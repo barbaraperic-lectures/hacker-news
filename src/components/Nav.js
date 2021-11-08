82ed41213bc2e21744b7715df6bef6e50b2f89dc
Barbara Peric <b.peric10@gmail.com>
2021-11-08 17:03:38 +0000

    add styled components

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+const Nav = () => {
+  return (
+    <div>
+      <p></p>
+      <nav>
+        <ul>
+          <li>about</li>
+          <li>mood</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav;
